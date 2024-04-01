b0396d53097ba9287ab5bba9e63ab3c3e59e18d2
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-04-01 14:08:25 +0300

    impl

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -11,69 +11,79 @@ class TodoRepositoryImpl implements TodoRepository {
   // Fetch all todos from the data source
   @override
   Future<List<TodoEntity>> getTodos() async {
-    final List<TodoModel> result = await _todoDataSource.getTodos();
-    // Convert data source models to domain entities and return them
-    return result
-        .map((e) => TodoEntity(
-              id: e.id,
-              description: e.description,
-              isCompleted: e.isCompleted,
-            ))
-        .toList();
+    try {
+      // Retrieve todos from data source
+      final List<TodoModel> result = await _todoDataSource.getTodos();
+      // Convert data source models to domain entities and return them
+      return result.map((e) => _mapModelToEntity(e)).toList();
+    } catch (e) {
+      // Throw an error if fetching todos fails
+      throw Exception('Failed to fetch todos: $e');
+    }
   }
 
   // Add a new todo to the data source
   @override
   Future<List<TodoEntity>> addTodo(TodoEntity todoEntity) async {
-    // Convert domain entity to data source model
-    final TodoModel todoModel = TodoModel(
-      id: todoEntity.id,
-      description: todoEntity.description,
-      isCompleted: todoEntity.isCompleted,
-    );
-    // Add the todo to the data source
-    final List<TodoModel> result = await _todoDataSource.addTodo(todoModel);
-    return result
-        .map((e) => TodoEntity(
-              id: e.id,
-              description: e.description,
-              isCompleted: e.isCompleted,
-            ))
-        .toList();
+    try {
+      // Convert domain entity to data source model
+      final TodoModel todoModel = _mapEntityToModel(todoEntity);
+      // Add the todo to the data source
+      final List<TodoModel> result = await _todoDataSource.addTodo(todoModel);
+      // Convert updated models back to entities and return them
+      return result.map((e) => _mapModelToEntity(e)).toList();
+    } catch (e) {
+      // Throw an error if adding todo fails
+      throw Exception('Failed to add todo: $e');
+    }
   }
 
   // Delete a todo from the data source by its ID
   @override
   Future<List<TodoEntity>> deleteTodo(String id) async {
-    // Delete the todo from the data source
-    final List<TodoModel> result = await _todoDataSource.deleteTodo(id);
-    return result
-        .map((e) => TodoEntity(
-              id: e.id,
-              description: e.description,
-              isCompleted: e.isCompleted,
-            ))
-        .toList();
+    try {
+      // Delete the todo from the data source
+      final List<TodoModel> result = await _todoDataSource.deleteTodo(id);
+      // Convert updated models back to entities and return them
+      return result.map((e) => _mapModelToEntity(e)).toList();
+    } catch (e) {
+      // Throw an error if deleting todo fails
+      throw Exception('Failed to delete todo: $e');
+    }
   }
 
   // Update a todo in the data source
   @override
   Future<List<TodoEntity>> updateTodo(TodoEntity todoEntity) async {
-    // Convert domain entity to data source model
-    final TodoModel todoModel = TodoModel(
-      id: todoEntity.id,
-      description: todoEntity.description,
-      isCompleted: todoEntity.isCompleted,
-    );
-    // Update the todo in the data source
-    final List<TodoModel> result = await _todoDataSource.updateTodo(todoModel);
+    try {
+      // Convert domain entity to data source model
+      final TodoModel todoModel = _mapEntityToModel(todoEntity);
+      // Update the todo in the data source
+      final List<TodoModel> result =
+          await _todoDataSource.updateTodo(todoModel);
+      // Convert updated models back to entities and return them
+      return result.map((e) => _mapModelToEntity(e)).toList();
+    } catch (e) {
+      // Throw an error if updating todo fails
+      throw Exception('Failed to update todo: $e');
+    }
+  }
 
-    return result
-        .map((e) => TodoEntity(
-              id: e.id,
-              description: e.description,
-              isCompleted: e.isCompleted,
-            ))
-        .toList();
+  // Helper method to map TodoModel to TodoEntity
+  TodoEntity _mapModelToEntity(TodoModel model) {
+    return TodoEntity(
+      id: model.id,
+      description: model.description,
+      isCompleted: model.isCompleted,
+    );
+  }
+
+  // Helper method to map TodoEntity to TodoModel
+  TodoModel _mapEntityToModel(TodoEntity entity) {
+    return TodoModel(
+      id: entity.id,
+      description: entity.description,
+      isCompleted: entity.isCompleted,
+    );
   }
 }
